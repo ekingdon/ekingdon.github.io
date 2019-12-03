fe9193d725d7d76c9c9954242d8cc202fc80f4c4
samlibby <57444905+samlibby@users.noreply.github.com>
2019-12-03 14:43:39 -0500

    Update northpolemap.js

--- northpolemap.js
+++ northpolemap.js
@@ -1,6 +1,7 @@
 // Reference: P. 442-447 in "Javascript & JQuery: Interactive Front-End Web Development" by Jon Duckett
+// Referred to https://www.w3schools.com/js/js_function_call.asp for additonal support
 
-function init()
+function init() {
   var mapOptions = {
     center: new google.maps.LatLng(64.7511563,-147.3793779),
     mapTypeID: google.maps.MapTypeId.ROADMAP,
@@ -8,6 +9,7 @@ function init()
     
     var venueMap;
     venueMap = new google.maps.Map (document.getElementByID('northpole'), mapOptions);
+}
 
 function loadScript(){ 
   var script = document.createElement('script');
