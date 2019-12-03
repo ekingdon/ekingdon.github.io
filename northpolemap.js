645d01add72748f7e8a6e97a48222b92d700a412
samlibby <57444905+samlibby@users.noreply.github.com>
2019-12-03 14:26:17 -0500

    Create northpolemap.js

---
+++ northpolemap.js
@@ -0,0 +1,17 @@
+// Reference: P. 442-447 in "Javascript & JQuery: Interactive Front-End Web Development" by Jon Duckett
+
+function init()
+  var mapOptions = {
+    center: new google.maps.LatLng(64.7511563,-147.3793779),
+    mapTypeID: google.maps.MapTypeId.ROADMAP,
+    zoom:13 };
+    
+    var venueMap;
+    venueMap = new google.maps.Map (document.getElementByID('northpole'), mapOptions);
+
+function loadScript(){ 
+  var script = document.createElement('script');
+  script.src = 'http://maps.googleapis.com/api/js?sensor=false&callback=init';
+  document.body.appendChild(script);
+  
+  window.onload = loadScript;
