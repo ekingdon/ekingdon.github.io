ca2fae2659b8f4ef0d921ae5cd9f092d526c05de
samlibby <57444905+samlibby@users.noreply.github.com>
2019-12-03 14:47:54 -0500

    Update northpolemap.js

--- northpolemap.js
+++ northpolemap.js
@@ -13,7 +13,7 @@ function init() {
 
 function loadScript(){ 
   var script = document.createElement('script');
-  script.src = 'http://maps.googleapis.com/api/js?sensor=false&callback=init';
+  script.src = 'https://maps.googleapis.com/api/js?sensor=false&callback=init';
   document.body.appendChild(script);
 }
   
