541237d963c849197c6e8a9fac96148c4a8bd02f
samlibby <57444905+samlibby@users.noreply.github.com>
2019-12-03 15:29:25 -0500

    Update northpolemap.js

--- northpolemap.js
+++ northpolemap.js
@@ -1,5 +1,11 @@
-// Reference: P. 442-447 in "Javascript & JQuery: Interactive Front-End Web Development" by Jon Duckett
+// Referred to P. 442-447 in "Javascript & JQuery: Interactive Front-End Web Development" by Jon Duckett
 // Referred to https://www.w3schools.com/js/js_function_call.asp for additonal support
+// Referred to https://www.sitepoint.com/google-maps-javascript-api-the-right-way/
+
+document.addEventListener('LoadContent', function ()
+  if (document.querySelectorAll('#northpole').length >0)
+
+{
 
 function init() {
   var mapOptions = {
@@ -8,13 +14,13 @@ function init() {
     zoom:13 };
     
     var venueMap;
-    venueMap = new google.maps.Map (document.getElementByID('northpole'), mapOptions);
+    venueMap = new google.maps.Map (document.getElementById('northpole'), mapOptions);
 }
 
 function loadScript(){ 
   var script = document.createElement('script');
+  script.type = 'text/javascript';
   script.src = 'https://maps.googleapis.com/api/js?key=AIzaSyBjZiWFPIlj4Gx4aa2YQsphCnOOVR5UBj8&callback=initMap';
-  document.body.appendChild(script);
-}
+  document.getElementByTagName('northpole')[0].appendChild(script); }}
   
-  window.onload = loadScript;
+  window.onload = loadScript);
