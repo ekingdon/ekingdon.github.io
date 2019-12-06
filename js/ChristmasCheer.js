31eb40b5f2c4623dafa52fb0ad0cdd57f5500f1b
becolga <57447715+becolga@users.noreply.github.com>
2019-12-06 14:35:44 -0500

    Add files via upload

---
+++ js/ChristmasCheer.js
@@ -0,0 +1,10 @@
+// Christmas Cheer Button Function - Ben Colgan
+//Sourced from: https://www.tutorialspoint.com/How-to-call-a-JavaScript-function-from-an-onClick-event
+
+function display() {
+	alert("Merry Christmas Hohohoho!! And also Happy Holidays!!!")
+}
+
+
+
+
